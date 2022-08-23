e479b30ff6ac894df9c130b2983e70693803d1f8
superman <duonga2qp@gmail.com>
2022-08-23 18:53:46 +0700

    finish login-regster

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const app = express();
 require('dotenv').config();
 const connectDB = require('./config/db');
 const cors = require('cors');
-const unless = require('express-unless');
+const {unless} = require('express-unless');
 
 const boardRouter = require('./routes/boardRoute');
 const listRouter = require('./routes/listRoute');
+const userRoute = require('./routes/userRoute');
+const auth = require("./middlewares/auth");
 
 
 const PORT = process.env.PORT || 5000;
@@ -17,13 +19,24 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-    return res.json("Ok")
-});
+
+// AUTH VERIFICATION AND UNLESS
+
+auth.verifyToken.unless = unless;
+
+app.use(
+    auth.verifyToken.unless({
+        path: [
+            {url: '/api/user/login', method: ['POST']},
+            {url: '/api/user/register', method: ['POST']},
+        ],
+    })
+);
 
 // Routes
 app.use("/api/board", boardRouter);
 app.use("/api/list", listRouter);
+app.use('/api/user', userRoute);
 
 connectDB();
 app.listen(PORT, () => {
