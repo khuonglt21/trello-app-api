a91b85545ed923edcc257fab7ef4aed921a20bf7
HaiPhu2702 <phukaht@gmail.com>
2022-09-05 10:12:54 +0700

    phan quyen admin viewer member

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -2,15 +2,11 @@ const express = require('express');
 const boardController = require('../controllers/boardController');
 const route = express.Router();
 
-
 route.get('/:id', boardController.getById);
 route.post('/:boardId/add-member', boardController.addMember);
 route.get('/:id/activity', boardController.getActivityById);
 route.put('/:boardId/update-board-title', boardController.updateBoardTitle);
 route.put('/:boardId/update-expanded-labels', boardController.updateIsExpandedLabels);
-route.put('/delete-member-in-board', boardController.deleteMember);
-
-
-
+route.put('/:boardId/delete-member', boardController.deleteMember);
 
 module.exports = route;
